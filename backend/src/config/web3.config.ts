b5f85a90835178254435bbf3c69f47c1132343eb
sean samuel <sinbad4u007@yahoo.com>
2025-11-17 14:03:16 +0100

    option for web providerurl

--- backend/src/config/web3.config.ts
+++ backend/src/config/web3.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('web3', () => ({
-  providerUrl: process.env.WEB3_PROVIDER_URL || 'https://mainnet.infura.io/v3/your-key',
+  providerUrl: process.env.WEB3_PROVIDER_URL || 'https://eth-sepolia.g.alchemy.com/v2/yeh5_cSGrKWM040A2LLfJ',
   contractAddress: process.env.CONTRACT_ADDRESS,
   privateKey: process.env.PRIVATE_KEY,
 }));
